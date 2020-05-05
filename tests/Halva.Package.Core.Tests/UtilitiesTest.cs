7bd1627d53d158a39b2b2e9da0d39473ed5bbb03
Dimitris Trachiotis <acemod13@outlook.com>
2020-05-05 19:02:33 +0300

    Added Compression Utilities Tests.

--- tests/Halva.Package.Core.Tests/UtilitiesTest.cs
+++ tests/Halva.Package.Core.Tests/UtilitiesTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Halva.Package.Core.Tests
@@ -6,9 +7,37 @@ namespace Halva.Package.Core.Tests
     public class UtilitiesTest
     {
         [Fact]
-        public void Test1()
+        public void CompressionCheck()
         {
+            bool finished = true;
+            if (Directory.Exists("SampleFiles1")) Directory.Delete("SampleFiles1", true);
+            try
+            {
+               PackageUtilities.CreateArchiveFromFolder("SampleFiles", "SampleFiles.halva");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                finished = false;
+            }
+            Assert.True(finished);
+        }
 
+        [Fact]
+        public void DecompressionCheck()
+        {
+            bool finished = true;
+            if (Directory.Exists("SampleFiles1")) Directory.Delete("SampleFiles1", true);
+            try
+            {
+                PackageUtilities.ExportFromArchive("SampleFiles.halva", "SampleFiles1");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                finished = false;
+            }
+            Assert.True(finished);
         }
     }
 }
