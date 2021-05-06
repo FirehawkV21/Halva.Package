abf04e989fed7240f7c3d8b4a39848a0994fe83b
Dimitris Trachiotis <acemod13@outlook.com>
2021-05-06 14:58:38 +0300

    Prep for beta.

--- src/Halva.Package.Core/Manager/HalvaPackageBase.cs
+++ src/Halva.Package.Core/Manager/HalvaPackageBase.cs
@@ -204,6 +204,8 @@ namespace Halva.Package.Core.Manager
         {
             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+
         }
     }
 }
